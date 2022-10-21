68b799f2b78550859cdbfcb208e5c47b3258d8d6
Jayson Chiang <jayson.chiang@outlook.com>
2022-10-21 12:49:09 +0800

    Custom Validators

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -6,35 +6,39 @@ import { UniqueUsername } from '../validators/unique-username';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css'],
+  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
   authForm = new FormGroup(
     {
-      username: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(20),
-        Validators.pattern(/^[a-z0-9]+$/),
-      ],[this.uniqueUserName.validate]),
+      username: new FormControl(
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+          Validators.pattern(/^[a-z0-9]+$/)
+        ],
+        [this.uniqueUsername.validate]
+      ),
       password: new FormControl('', [
         Validators.required,
         Validators.minLength(4),
-        Validators.maxLength(20),
+        Validators.maxLength(20)
       ]),
       passwordConfirmation: new FormControl('', [
         Validators.required,
         Validators.minLength(4),
-        Validators.maxLength(20),
-      ]),
+        Validators.maxLength(20)
+      ])
     },
     { validators: [this.matchPassword.validate] }
   );
 
   constructor(
     private matchPassword: MatchPassword,
-    private uniqueUserName: UniqueUsername
+    private uniqueUsername: UniqueUsername
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {}
 }
