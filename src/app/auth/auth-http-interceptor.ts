096fe03787f9896a62caa076af21692ba2370dad
Jayson Chiang <jayson.chiang@outlook.com>
2022-12-07 19:23:09 +0800

    Handling Authentication

---
+++ src/app/auth/auth-http-interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable()
+export class AuthHttpInterceptor implements HttpInterceptor {
+  // Modify of log the outgoing request
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const modifiedReq = req.clone({
+      withCredentials: true,
+    });
+
+    return next.handle(modifiedReq)
+    ;
+  }
+}
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,22 +1,83 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 interface UsernameAvailableResponse {
   available: boolean;
 }
 
+interface SignupCrednetails {
+  username: string;
+  passowrd: string;
+  passwordConfirmation: string;
+}
+
+interface SigninCredentails {
+  username: string;
+  passowrd: string;
+}
+
+interface SignupResponse {
+  username: string;
+}
+
+interface SignedinResponse {
+  authenticated: boolean;
+  username: string;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  rootUrl = 'https://api.angular-email.com';
+  signedin$ = new BehaviorSubject(false);
+
   constructor(private http: HttpClient) {}
 
   usernameAvailable(username: string) {
     return this.http.post<UsernameAvailableResponse>(
-      'https://api.angular-email.com/auth/username',
+      `${this.rootUrl}/auth/username`,
       {
-        username
+        username,
       }
+    );
+  }
+
+  signup(credentials: SignupCrednetails) {
+    return this.http
+      .post<SignupResponse>(`${this.rootUrl}/auth/signup`, credentials)
+      .pipe(
+        tap(() => {
+          this.signedin$.next(true);
+        })
+      );
+  }
+
+  checkAuth() {
+    return this.http
+      .get<SignedinResponse>(`${this.rootUrl}/auth/signedin`)
+      .pipe(
+        tap(({ authenticated }) => {
+          this.signedin$.next(authenticated);
+        })
+      );
+  }
+
+  signout() {
+    return this.http.post(`${this.rootUrl}/auth/signout`, {}).pipe(
+      tap(() => {
+        this.signedin$.next(false);
+      })
+    );
+  }
+
+  signin(credentials: SigninCredentails) {
+    return this.http.post(`${this.rootUrl}/auth/signin`, credentials).pipe(
+      tap(() => {
+        this.signedin$.next(true);
+      })
     );
   }
 }
